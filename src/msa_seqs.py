dfdbd8a87b190772b95b02462cf38b7c457e8d2e
dlrsb <id7215@alunos.uminho.pt>
2024-01-16 17:15:23 +0000

    Removed code that counts the # of sequences in the raw MSA hits files

--- src/msa_seqs.py
+++ src/msa_seqs.py
@@ -29,20 +29,20 @@ def get_num_msa_hits(results_dir):
         results['num_alignments'] = str(features['num_alignments'].tolist())
     del features
 
-    msas_dir = os.path.join(results_dir, 'msas')
-    chains = [x for x in os.listdir(msas_dir) if os.path.isdir(os.path.join(msas_dir, x))]
-    for chain in chains:
-        chain_dir = os.path.join(msas_dir, chain)
-        alignment_files = os.listdir(chain_dir)
-        for alignment_file in alignment_files:
-            name, extension = os.path.split(alignment_file)[-1].split('.')
-            try:
-                if extension == 'a3m':
-                    results['num_{db}_{chain}'.format(db=name, chain=chain)] = count_hits_a3m(os.path.join(chain_dir, alignment_file))
-                else:
-                    results['num_{db}_{chain}'.format(db=name, chain=chain)] = count_hits_sto(os.path.join(chain_dir, alignment_file))
-            except Exception as e:
-                print(e)
-                results['num_{db}_{chain}'.format(db=name, chain=chain)] = np.nan
+    # msas_dir = os.path.join(results_dir, 'msas')
+    # chains = [x for x in os.listdir(msas_dir) if os.path.isdir(os.path.join(msas_dir, x))]
+    # for chain in chains:
+    #     chain_dir = os.path.join(msas_dir, chain)
+    #     alignment_files = os.listdir(chain_dir)
+    #     for alignment_file in alignment_files:
+    #         name, extension = os.path.split(alignment_file)[-1].split('.')
+    #         try:
+    #             if extension == 'a3m':
+    #                 results['num_{db}_{chain}'.format(db=name, chain=chain)] = count_hits_a3m(os.path.join(chain_dir, alignment_file))
+    #             else:
+    #                 results['num_{db}_{chain}'.format(db=name, chain=chain)] = count_hits_sto(os.path.join(chain_dir, alignment_file))
+    #         except Exception as e:
+    #             print(e)
+    #             results['num_{db}_{chain}'.format(db=name, chain=chain)] = np.nan
 
     return results
